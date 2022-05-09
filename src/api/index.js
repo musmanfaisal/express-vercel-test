9dd1ffa81b3cbdd0c35feb2396f7e7a4374b68b4
usmanfaisal60 <usman.faisal60@gmail.com>
2022-05-09 19:07:23 +0500

    - added some tweaks

--- src/api/index.js
+++ src/api/index.js
@@ -9,13 +9,13 @@ const bids = require('../controllers/bids');
 const routersInit = () => {
     const router = express();
     // register api points
-    router.use('/bid' , bids(services));
-    
+    router.use('/bid', bids(services));
+    router.get("/test", (req, res) => {
+        res.json({ success: true, message: "Testing mango speaking" })
+    })
+
     // catch api all errors
     router.use(errorHandler);
-    router.get("/test", (req, res) => {
-        res.json({success: true, message: "Testing mango speaking"})
-    })
     return router;
 };
 
